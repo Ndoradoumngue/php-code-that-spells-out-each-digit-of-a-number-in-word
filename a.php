8e5fb85a7a359a4b1ba61e6c11fe0297bd5b73d7
Christian Ndoradoumngue <christianndoradoumngue@rocketmail.com>
2016-10-11 14:57:40 +0300

    Update a.php

--- a.php
+++ a.php
@@ -1,30 +1,44 @@
 <?php 
 
+//Function start
 
 function num_toWord($param)
 {
+	//Make sure to surrounder input number with quotes to read it as an array
 	$num = "'".$param."'";
-   
-$arrayName = array(' zero ', ' one ', ' two ', ' three ', ' four ', ' five ', ' six ', ' seven ', ' eight ', ' nine ');
+	
+	//Preparing array of numbers in word to output   
+	$arrayName = array(' zero ', ' one ', ' two ', ' three ', ' four ', ' five ', ' six ', ' seven ', ' eight ', ' nine ');
 
-$nbrLength = strlen($num)-1;
+	//Getting the length of the input number
+	$nbrLength = strlen($num)-1;
+	
+	//For each diggit in the number
+	for ($i=0; $i < $nbrLength; $i++) { 
+		
+		//Loop untill the diggit to compare it's index with the array of words		
 
+		for ($j=1; $j <= $num[$i]; $j++) { 
+			
+			//Get the diggit in word
+			$inWord = $arrayName[$j];
+		}
+		
+		//Appending the diggits in word as output
+		$output .= $inWord;
 
-for ($i=0; $i < $nbrLength; $i++) { 
+	}
+	
+	//Returning the output
 
-for ($j=1; $j <= $num[$i]; $j++) { 
+	return $output;
 
-	$inWord = $arrayName[$j];
 }
 
-$output .= $inWord;
+//End function
 
-}
 
-echo $output;
-
-}
-
+//Function call
 echo num_toWord(4367);
 
 
